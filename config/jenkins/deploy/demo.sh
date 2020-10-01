1cf508a884b633df7a899273b3a58ccb3a3bb49a
Ratha Heang <ratha@web-essentials.asia>
2020-10-01 09:32:15 +0700

    [TASK] Add deployment script
    refs SS-00

---
+++ config/jenkins/deploy/demo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+exit 0
+FILE_NAME=$(basename "$0")
+STAGE="${FILE_NAME%.*}"
+
+ansible-playbook config/ansible/playbook.yml \
+    -l ${STAGE} \
+    -e stage=${STAGE} \
+    -e version=${DEPLOY_ID}
---
+++ config/jenkins/uat/demo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "TODO: Implement UAT to check interface"
