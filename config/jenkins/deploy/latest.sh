36b1da891012bf1edd932301330e324ddad8edf7
Lysa Thorn <lysa@web-essentials.co>
2021-03-25 10:07:47 +0700

    [TASK] Execute script for testing
    refs TRA-303

--- config/jenkins/deploy/latest.sh
+++ config/jenkins/deploy/latest.sh
@@ -3,7 +3,7 @@
 FILE_NAME=$(basename "$0")
 STAGE="${FILE_NAME%.*}"
 
-ansible-playbook config/ansible/playbook.yml -vv\
+ansible-playbook config/ansible/playbook.yml -t deploy -vv\
     -l ${STAGE} \
     -e stage=${STAGE} \
     -e version=${DEPLOY_ID}
--- config/jenkins/uat/demo.sh
+++ config/jenkins/uat/demo.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "TODO: Implement UAT to check interface"
+FILE_NAME=$(basename "$0")
+STAGE="${FILE_NAME%.*}"
+
+ansible-playbook config/ansible/playbook.yml -t feature -vv\
+    -l ${STAGE} \
+    -e stage=${STAGE} \
+    -e version=${DEPLOY_ID}
