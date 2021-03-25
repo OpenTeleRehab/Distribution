8ccc271a4c0441ebc5d39b06d21a6037efcf687c
Ratha Heang <ratha@web-essentials.co>
2021-03-25 12:03:09 +0700

    [INFRA] Add laravel dusk

--- config/jenkins/deploy/laravel_dusk.sh
+++ config/jenkins/deploy/laravel_dusk.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-cd /home/jenkins/jenkins-slave/workspace/hiv-tra-20-rehabilitation.wehost.asia/test/admin-service && /opt/php-versions/php74/bin/php artisan dusk
+WORKSPACE="/home/jenkins/jenkins-slave/workspace/hiv-tra-20-rehabilitation.wehost.asia/test/admin-service"
+
+echo "Composer install"
+cd ${WORKSPACE} && /opt/php-versions/php74/bin/php /usr/local/bin/composer install
+
+echo "Laravel dusk"
+cd ${WORKSPACE} && /opt/php-versions/php74/bin/php artisan dusk
