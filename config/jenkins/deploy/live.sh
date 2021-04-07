58fc17943e71801a5f5c88f3cbc94d529c4a0a87
Sophea Phy <sophea@web-essentials.co>
2021-04-07 10:28:19 +0700

    [INFRA] Add live stage

--- config/jenkins/deploy/live.sh
+++ config/jenkins/deploy/live.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-exit 0
 FILE_NAME=$(basename "$0")
 STAGE="${FILE_NAME%.*}"
 
-ansible-playbook config/ansible/playbook.yml -t deploy \
+ansible-playbook config/ansible/playbook.yml -t deploy -vv\
     -l ${STAGE} \
     -e stage=${STAGE} \
     -e version=${DEPLOY_ID}
