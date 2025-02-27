153af131704cdc8171febef949690e70280a1582
Longheng Chhoeun <longheng@web-essentials.co>
2025-02-27 17:38:20 +0700

    [IMP] Implement with redis in superset
    refs TRA-947

--- config/docker/superset/superset_config.py
+++ config/docker/superset/superset_config.py
@@ -4,3 +4,12 @@ FEATURE_FLAGS = {
 }
 
 GUEST_ROLE_NAME= 'embed_dashboard'
+
+CACHE_CONFIG = {
+    "CACHE_TYPE": "redis",
+    "CACHE_DEFAULT_TIMEOUT": 300,
+    "CACHE_KEY_PREFIX": "superset_",
+    "CACHE_REDIS_URL": "redis://superset_cache:6379/0",
+}
+
+DATA_CACHE_CONFIG = CACHE_CONFIG
